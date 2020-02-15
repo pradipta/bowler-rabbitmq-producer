f83eaca18b273450e3f238d42abbe711e67ce89b
Pradipta Sarma <pradipta.sarma@UE-B-NOTE-0046.local>
2020-02-15 10:06:15 +0530

    dockerfile

--- src/main/java/com/pradipta/bowlerrabbitmqproducer/rabbitmq/producer/ProducerController.java
+++ src/main/java/com/pradipta/bowlerrabbitmqproducer/rabbitmq/producer/ProducerController.java
@@ -7,15 +7,17 @@ import com.pradipta.bowlerrabbitmqproducer.rabbitmq.producer.fanoutQueueProducer
 import com.pradipta.bowlerrabbitmqproducer.rabbitmq.producer.topicQueueProducer.ConfigureTopicRabbitMQ;
 import lombok.AllArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
 public class ProducerController {
     private final RabbitTemplate rabbitTemplate;
+
+    @GetMapping("/hello")
+    public String getHello(){
+        return "Hello";
+    }
 
     @RequestMapping(method = RequestMethod.POST, value = "/sendmessage/topic")
     public String sendTopicMessage(@RequestBody OrderRequest orderRequest){
